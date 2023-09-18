3fc3946cc97f9279b49e87a43519367349fc0642
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2023-09-18 11:00:54 -0400

    Initial Commit

--- exercise_ra.py
+++ exercise_ra.py
@@ -1,6 +1,7 @@
 import ra_operator as RA;
 import os
 import csv
+import test_operators
 
 # Data source: https://relational.fit.cvut.cz/dataset/IMDb
 # Information courtesy of IMDb (http://www.imdb.com). Used with permission.
@@ -39,41 +40,80 @@ def main():
     movies = open_file(filepath + '/movies.csv')
     roles = open_file(filepath + '/roles.csv')
 
+
+    #Test cases:
+    test_operators.test_union_0()
+    test_operators.test_union_1()
+    test_operators.test_union2_0()
+    test_operators.test_union2_1()
+    test_operators.test_union2_2()
+    test_operators.test_intersection_0()
+    test_operators.test_intersection_1()
+    test_operators.test_intersection_2()
+    test_operators.test_set_diff_0()
+    test_operators.test_set_diff_1()
+    test_operators.test_set_diff_2()
+    test_operators.test_selection_0()
+    test_operators.test_project_0()
+    test_operators.test_project_1()
+    test_operators.test_project_2()
+
     # 1- Perform the union between the actors and extra_actors relation
     # This operation should returns all of the rows of actor and extra_actors unioned in to a single list. 
 
     ##ADD YOUR CODE HERE
+    unioned_list = RA.union(actors, extra_actors)
+    print(unioned_list)
 
     # 2- Show all tuples from actors that first name is Chris
     
     ##ADD YOUR CODE HERE
+    chris_list = RA.selection(actors, 1, "Chris", "==")
+    print(chris_list)
 
     # 3 - Show all tuples from movies that were made after 1998 
     
     ##ADD YOUR CODE HERE
+    movies_list_1998 = RA.selection(movies, 2, "1998", ">")
+    print(movies_list_1998)
 
     # 4 - Show all tuples from actors that are female AND id is bigger than 200000
     
     ##ADD YOUR CODE HERE
+    female_actors = RA.intersection(RA.selection(actors, 3, "F", "=="), RA.selection(actors, 0, "200000", ">"))
+    print(female_actors)
 
     # 5 - Find the last name of all actors named Chris
     
     ##ADD YOUR CODE HERE
+    last_name = RA.project(chris_list, [2])
+    print(last_name)
 
 
     # 6 - Find the names of the movies that were made after 1998 
     
     ##ADD YOUR CODE HERE
+    movies_name = RA.project(movies_list_1998, [1])
+    print(movies_name)
     
     # 7 - Find the name of actors that are female AND id is bigger than 200000
     
     ##ADD YOUR CODE HERE
+    female_actors_names = RA.project(female_actors, [1, 2])
+    print(female_actors_names)
     
     # 8 -  Take the cross product of actors and movies
     
     ##ADD YOUR CODE HERE
+    actor_movie_cross = RA.crossproduct(actors, movies)
+    for row in actor_movie_cross:
+        print(row)
 
     # 8 -  Print the name of actors and the name of movies they have had a role in it.
     ##ADD YOUR CODE HERE
+    actor_movie_role_cross = RA.crossproduct(actor_movie_cross, roles)
+    for row in actor_movie_role_cross:
+        if(row[0]==row[8] and row[4]==row[9]):
+           print(row[1]+", "+row[2]+", "+row[5])
 
 main()
--- ra_operator.py
+++ ra_operator.py
@@ -10,21 +10,36 @@ def union(lst1, lst2):
     """ 
      ## IMPLEMENT HERE THE UNION OF TWO LISTS
 
-    return 
+    return lst1 + lst2
 
 # mplement an operator to merge two lists with no duplicated allowed
 def union2(lst1, lst2):
     """Relational algebra project set union with no duplicates
     """ 
     ###IMPLEMENT HERE THE INTERSECTION OF TWO LISTS
-    return 
+    union_list = lst1 + lst2
+    unique_actors_set = set()
+    unique_actors_list = []
+
+    for actor in union_list:
+        actor_tuple = tuple(actor)
+        if actor_tuple not in unique_actors_set:
+            unique_actors_list.append(actor)
+            unique_actors_set.add(actor_tuple)
+    return unique_actors_list 
 
 # Implement an operator to perform the intersection of two lists
 def intersection(lst1, lst2):
     """Relational algebra project set intersection
     """ 
     ###IMPLEMENT HERE THE INTERSECTION OF TWO LISTS
-    return 
+    set_lst1 = {tuple(inner_list) for inner_list in lst1}
+    set_lst2 = {tuple(inner_list) for inner_list in lst2}
+
+    intersection_set = set_lst1.intersection(set_lst2)
+
+    result_list = [list(inner_tuple) for inner_tuple in intersection_set]
+    return result_list 
 
 
 # Implement an operator to perform the set difference of two lists
@@ -32,7 +47,13 @@ def set_difference(lst1, lst2):
     """Relational algebra project set difference
     """ 
     ## IMPLEMENT HERE THE SET DIFFERENT BETWEEN TWO LISTS
-    return 
+    set_lst1 = {tuple(inner_list) for inner_list in lst1}
+    set_lst2 = {tuple(inner_list) for inner_list in lst2}
+
+    intersection_set = set_lst1.difference(set_lst2)
+
+    result_list = [list(inner_tuple) for inner_tuple in intersection_set]
+    return result_list 
 
 ## RA SELECTIOn
 
@@ -40,7 +61,48 @@ def selection(relation, column, predicate, mode):
     """Relational algebra project selection
     """ 
     ## IMPLEMENT HERE THE SELECTION OPERATOR valid modes are ['<', '<=', '>', '>=', '==', '!=']
-    return 
+    match mode:
+        case '<':
+            return_list = []
+            for row in relation:
+                if row[column] < predicate:
+                    return_list.append(row)
+            return return_list
+
+        case '<=':
+            return_list = []
+            for row in relation:
+                if row[column] <= predicate:
+                    return_list.append(row)
+            return return_list
+
+        case '>':
+            return_list = []
+            for row in relation:
+                if row[column] > predicate:
+                    return_list.append(row)
+            return return_list
+
+        case '>=':
+            return_list = []
+            for row in relation:
+                if row[column] >= predicate:
+                    return_list.append(row)
+            return return_list
+
+        case '==':
+            return_list = []
+            for row in relation:
+                if row[column] == predicate:
+                    return_list.append(row)
+            return return_list
+
+        case '!=':
+            return_list = []
+            for row in relation:
+                if row[column] != predicate:
+                    return_list.append(row)
+            return return_list 
 
 #RA PROJECTION
 
@@ -52,7 +114,14 @@ def project(relation, columns):
     """
     # IMPLEMENT HERE THE PROJECT OPERATOR. THE FUNCTION CCEPTS A LIST OF COLUMNS INDEXES AND RETURN THE VALUES OF THOSE COLUMNS 
     
-    return 
+    return_list = []
+    for row in relation:
+        rows_list = []
+        for i in range(len(columns)):
+            rows_list.append(row[columns[i]])
+        return_list.append(rows_list)
+
+    return return_list 
 
 ## CROSS PRODUCT
 # The cross product pairs each row of a relation with every row of another relation to create 
@@ -62,4 +131,5 @@ def crossproduct(relation_a, relation_b):
     """Relational algebra cross product operator
     """
     ## IMPLEMENT CROSS PRODUCT HERE
-    return 
+    return [a + b for a in relation_a for b in relation_b]
+ 
